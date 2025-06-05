ee0596595b57dfb5f0f89475fb58c49f1c844e09
wwwyssa <grisha.maryin@gmail.com>
2025-06-06 00:49:48 +0300

    upd

--- programming/untitled1/src/Main.java
+++ programming/untitled1/src/Main.java
@@ -1,15 +1,29 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.beans.EventHandler;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Stream.of("micro", "nano", "pico", "femto", "atto", "zepto", "yocto")
-                .filter(s -> s.length() == 5)
-                .map(s -> Main.del(s))
+        Stream.of("rat", "ox", "tiger", "rabbit", "dragon", "snake")
+                .filter(s -> s.length() > 4)
                 .limit(2)
                 .sorted()
                 .forEachOrdered(System.out::print);
+
+        TextArea myControl = new TextArea("Return of the Jedi");
+        myControl.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent event) {
+                this.setBackground(Color.YELLOW);
+            }
+
+            private void setBackground(Color yellow) {
+            }
+        });
+
+
     }
 
     public static String del(String input) {
